17315004f7203ec89419b6b4a39f4d1ca373d9d0
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-27 23:02:39 -0500

    my master

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Form extends Component {
   render() {
     return (
       <div>
         <form onSubmit={this.props.getWeather}>
-            <input type="text" name="city" id="city"/>
-            <input type="text" name="country" id="country"/>
-            <button>Get Weather</button>
+          <input type="text" name="city" id="city" />
+          <input type="text" name="country" id="country" />
+          <button>Get Weather</button>
         </form>
       </div>
-    )
+    );
   }
 }
